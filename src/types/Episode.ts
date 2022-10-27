a0c3f075ae43b56e94b1a7202f33f7429614d021
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2022-10-27 19:26:29 -0300

    chore: add episode interface

---
+++ src/types/Episode.ts
@@ -0,0 +1,11 @@
+export interface Episode {
+    id: number
+    name: string
+    seasonId: number
+    duration: number
+    path: string
+    position: number
+    createdAt: Date
+    updatedAt: Date
+    deletedAt: Date
+}
