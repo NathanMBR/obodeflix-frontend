35fe86d912a78b9dcab9be9a3862b5275e46ca11
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2022-10-28 15:40:39 -0300

    refactor: improve readability at get duration time helper

--- src/helpers/getDurationTime.ts
+++ src/helpers/getDurationTime.ts
@@ -1,1 +1,22 @@
-export const getDurationTime = (duration: number) => `${Math.floor(duration / 60) < 10 ? "0" : ""}${Math.floor(duration / 60)}min${duration % 60 < 10 ? "0" : ""}${duration % 60}s`;
+export const getDurationTime = (durationInSeconds: number) => {
+    const oneHourInSeconds = 3600;
+    const oneMinuteInSeconds = 60;
+
+    const hours = Math.floor(durationInSeconds / oneHourInSeconds);
+    const minutes = Math.floor((durationInSeconds % oneHourInSeconds) / oneMinuteInSeconds);
+    const seconds = Math.floor(durationInSeconds % oneMinuteInSeconds);
+
+    const hoursString = hours > 0
+        ? (hours < 10 ? `0${hours}h` : `${hours}h`)
+        : "";
+
+    const minutesString = minutes > 0
+        ? (minutes < 10 ? `0${minutes}min` : `${minutes}min`)
+        : (hours > 0 ? "00min" : "");
+
+    const secondsString = seconds > 0
+        ? (seconds < 10 ? `0${seconds}s` : `${seconds}s`)
+        : "00s";
+
+    return `${hoursString}${minutesString}${secondsString}`;
+}
