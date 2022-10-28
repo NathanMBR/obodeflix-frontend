2220097436267b1325e895d22b951957b86fcc79
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2022-10-28 20:00:56 -0300

    chore: add episode card component

---
+++ src/components/EpisodeCard/EpisodeCard.tsx
@@ -0,0 +1,45 @@
+import { getDurationTime } from "../../helpers";
+import { Episode } from "../../types";
+import {
+    Button,
+    Card,
+    CardActions,
+    CardContent,
+    Divider,
+    Grid,
+    Paper,
+    Tooltip,
+    Typography
+} from "@mui/material";
+
+export interface EpisodeCardProps {
+    episode: Episode;
+}
+
+export const EpisodeCard = (props: EpisodeCardProps) => {
+    const { episode } = props;
+
+    return (
+        <>
+            <Grid item xs={3} alignItems="stretch" key={episode.id}>
+                <Paper elevation={12}>
+                    <Card>
+                        <CardContent>
+                            <Typography variant="h5" component="h3">{episode.name}</Typography>
+                            <Typography variant="body2">Duração: {getDurationTime(episode.duration)}</Typography>
+                        </CardContent>
+                        <Divider />
+                        <CardActions>
+                            <Button>Baixar episódio</Button>
+                            <Tooltip title="Indisponível">
+                                <div>
+                                    <Button disabled>Assistir episódio</Button>
+                                </div>
+                            </Tooltip>
+                        </CardActions>
+                    </Card>
+                </Paper>
+            </Grid>
+        </>
+    );
+}
