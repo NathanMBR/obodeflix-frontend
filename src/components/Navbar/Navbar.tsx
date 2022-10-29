483dc89cb5a99ac3a1aee6deea54586c85fdda26
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2022-10-29 12:57:24 -0300

    chore: use router links at navbar buttons

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,18 +5,32 @@ import {
     Toolbar,
     Typography
 } from "@mui/material";
+import { Link } from "react-router-dom";
 
 export const Navbar = () => {
     return (
         <>
             <AppBar position="fixed">
                 <Toolbar>
-                    <Typography variant="h4" component="div" sx={{ flexGrow: 1 }}>OBODEFLIX</Typography>
+                    <Typography variant="h4" component="div" sx={{ flexGrow: 1 }}>
+                        <Link to="/">
+                            OBODEFLIX
+                        </Link>
+                    </Typography>
 
                     <Stack direction="row" spacing={2}>
-                        <Button color="inherit">Series</Button>
-                        <Button color="inherit">Login</Button>
-                        <Button color="inherit" variant="outlined">Sign up</Button>
+                        <Link to="/">
+                            <Button color="inherit">Home</Button>
+                        </Link>
+                        <Link to="/series">
+                            <Button color="inherit">Series</Button>
+                        </Link>
+                        <Link to="/login">
+                            <Button color="inherit">Login</Button>
+                        </Link>
+                        <Link to="/signup">
+                            <Button color="inherit" variant="outlined">Sign up</Button>
+                        </Link>
                     </Stack>
                 </Toolbar>
             </AppBar>
