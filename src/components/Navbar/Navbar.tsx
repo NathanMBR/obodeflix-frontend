622bb2d1204b6e71b6a12250f90b33f4eafce00a
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2022-10-30 14:03:05 -0300

    feat: theme selection

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,31 +5,41 @@ import {
     Toolbar,
     Typography
 } from "@mui/material";
+import { DarkMode } from "@mui/icons-material";
 import { Link } from "react-router-dom";
 
-export const Navbar = () => {
+export interface NavbarProps {
+    handleThemeChange: () => void
+};
+
+export const Navbar = (props: NavbarProps) => {
+    const { handleThemeChange } = props;
+
     return (
         <>
             <AppBar position="fixed">
                 <Toolbar>
-                    <Typography variant="h4" component="div" sx={{ flexGrow: 1 }}>
+                    <Typography variant="h4" component="h1" sx={{ flexGrow: 1 }}>
                         <Link to="/">
                             OBODEFLIX
                         </Link>
                     </Typography>
 
                     <Stack direction="row" spacing={2}>
+                        <Button color="inherit" onClick={handleThemeChange}>
+                            <DarkMode />
+                        </Button>
                         <Link to="/">
                             <Button color="inherit">Home</Button>
                         </Link>
                         <Link to="/series">
-                            <Button color="inherit">Series</Button>
+                            <Button color="inherit">Séries</Button>
                         </Link>
                         <Link to="/login">
-                            <Button color="inherit">Login</Button>
+                            <Button color="inherit">Fazer login</Button>
                         </Link>
                         <Link to="/signup">
-                            <Button color="inherit" variant="outlined">Sign up</Button>
+                            <Button color="inherit" variant="outlined">Criar conta</Button>
                         </Link>
                     </Stack>
                 </Toolbar>
