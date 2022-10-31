ff7f3a500bd52dd6c2293325eb4211c6872b0c50
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2022-10-31 12:47:11 -0300

    fix: persist chosen theme after refreshing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,64 @@
 import { ThemeProvider, createTheme } from "@mui/material/styles"
-import CssBaseline from "@mui/material/CssBaseline";
-import { Container } from "@mui/material";
+import { Container, CssBaseline } from "@mui/material";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
 
 import {
   Navbar,
   Footer
 } from "./components";
 import {
-  Main
+  Main,
+  SignUp
 } from "./pages"
 
-const themes = {
-  darkTheme: createTheme(
-    {
-      palette: {
-        mode: "dark"
-      }
-    }
-  ),
+import "./App.css"
 
-  lightTheme: createTheme(
+const themes = {
+  light: createTheme(
     {
       palette: {
         mode: "light"
       }
     }
+  ),
+
+  dark: createTheme(
+    {
+      palette: {
+        mode: "dark"
+      }
+    }
   )
 }
 
+type Theme = "light" | "dark";
+
 function App() {
-  const theme = localStorage.getItem("theme") === "light" ? themes.lightTheme : themes.darkTheme
+  const [theme, setTheme] = useState<Theme>(localStorage.getItem("theme") as Theme || "light");
+
+  const handleThemeChange = () => {
+    const newTheme = theme === "light" ? "dark" : "light"
+    setTheme(newTheme);
+    localStorage.setItem("theme", newTheme);
+  }
 
   return (
     <>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Navbar />
+      <ThemeProvider theme={themes[theme]}>
+        <BrowserRouter>
+          <CssBaseline />
+          <Navbar handleThemeChange={handleThemeChange} />
 
-        <Container style={{ marginTop: 96, marginBottom: 64 }}>
-          <Main />
-        </Container>
+          <Container style={{ marginTop: 96, marginBottom: 64 }}>
+            <Routes>
+              <Route path="/" element={<Main />} />
+              <Route path="/signup" element={<SignUp />} />
+            </Routes>
+          </Container>
 
-        <Footer />
+          <Footer />
+        </BrowserRouter>
       </ThemeProvider>
     </>
   )
