6f6b710edc9d4f185fbf5e0baf6866c063309a43
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2022-11-02 20:06:39 -0300

    chore: add pagination class

--- src/types/Pagination.ts
+++ src/types/Pagination.ts
@@ -1,7 +1,19 @@
-export interface Pagination<T extends Record<string, any>> {
+import { GenericObject } from "./GenericObject";
+
+export interface Pagination<T extends GenericObject> {
     readonly quantityPerPage: number
     readonly totalQuantity: number
     readonly currentPage: number
     readonly lastPage: number
     readonly data: Array<T>
+}
+
+export class Pagination<T extends GenericObject> implements Pagination<T> {
+    constructor(
+        public readonly quantityPerPage: number,
+        public readonly totalQuantity: number,
+        public readonly currentPage: number,
+        public readonly lastPage: number,
+        public readonly data: Array<T>
+    ) { }
 }
