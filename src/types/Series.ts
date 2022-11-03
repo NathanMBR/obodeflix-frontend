dfe98d9f24bbdb4dcb3ebfac1ee30d5f80ec00c8
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2022-11-03 16:09:01 -0300

    chore: correctly set nullable properties at series type

--- src/types/Series.ts
+++ src/types/Series.ts
@@ -4,28 +4,28 @@ export interface Series {
     readonly id: number;
 
     readonly mainName: string;
-    readonly alternativeName?: string;
+    readonly alternativeName: string | null;
     readonly mainNameLanguage: SeriesNameLanguages;
-    readonly description?: string;
-    readonly imageAddress?: string;
+    readonly description: string | null;
+    readonly imageAddress: string | null;
 
     readonly createdAt: Date;
     readonly updatedAt: Date;
-    readonly deletedAt?: Date;
+    readonly deletedAt: Date | null;
 }
 
 export class SeriesBuilder implements Series {
     public readonly id: Series["id"];
 
     public readonly mainName: Series["mainName"];
-    public readonly alternativeName?: Series["alternativeName"];
+    public readonly alternativeName: Series["alternativeName"];
     public readonly mainNameLanguage: Series["mainNameLanguage"];
-    public readonly description?: Series["description"];
-    public readonly imageAddress?: Series["imageAddress"];
+    public readonly description: Series["description"];
+    public readonly imageAddress: Series["imageAddress"];
 
     public readonly createdAt: Series["createdAt"];
     public readonly updatedAt: Series["updatedAt"];
-    public readonly deletedAt?: Series["deletedAt"];
+    public readonly deletedAt: Series["deletedAt"];
 
     constructor(
         private readonly seriesData: Series
