dc7613ad69a7fde29c8a515d465fb95a65b23544
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2022-11-03 16:11:53 -0300

    chore: correctly declare user and pagination types builder classes

--- src/types/Pagination.ts
+++ src/types/Pagination.ts
@@ -8,12 +8,20 @@ export interface Pagination<T extends GenericObject> {
     readonly data: Array<T>
 }
 
-export class Pagination<T extends GenericObject> implements Pagination<T> {
+export class PaginationBuilder<T extends GenericObject> implements Pagination<T> {
+    public readonly quantityPerPage: Pagination<T>["quantityPerPage"];
+    public readonly totalQuantity: Pagination<T>["totalQuantity"];
+    public readonly currentPage: Pagination<T>["currentPage"];
+    public readonly lastPage: Pagination<T>["lastPage"];
+    public readonly data: Pagination<T>["data"];
+
     constructor(
-        public readonly quantityPerPage: number,
-        public readonly totalQuantity: number,
-        public readonly currentPage: number,
-        public readonly lastPage: number,
-        public readonly data: Array<T>
-    ) { }
+        private readonly paginationData: Pagination<T>
+    ) {
+        this.quantityPerPage = paginationData.quantityPerPage;
+        this.totalQuantity = paginationData.totalQuantity;
+        this.currentPage = paginationData.currentPage;
+        this.lastPage = paginationData.lastPage;
+        this.data = paginationData.data;
+    }
 }
--- src/types/User.ts
+++ src/types/User.ts
@@ -12,16 +12,26 @@ export interface User {
     readonly deletedAt: Date | null;
 }
 
-export class User implements User {
+export class UserBuilder implements User {
+    public readonly id: User["id"];
+
+    public readonly name: User["name"];
+    public readonly email: User["email"];
+    public readonly type: User["type"];
+
+    public readonly createdAt: User["createdAt"];
+    public readonly updatedAt: User["updatedAt"];
+    public readonly deletedAt: User["deletedAt"];
+
     constructor(
-        public readonly id: number,
-
-        public readonly name: string,
-        public readonly email: string,
-        public readonly type: UserTypes,
-
-        public readonly createdAt: Date,
-        public readonly updatedAt: Date,
-        public readonly deletedAt: Date | null
-    ) { }
+        private readonly userData: User
+    ) {
+        this.id = userData.id;
+        this.name = userData.name;
+        this.email = userData.email;
+        this.type = userData.type;
+        this.createdAt = userData.createdAt;
+        this.updatedAt = userData.updatedAt;
+        this.deletedAt = userData.deletedAt;
+    }
 }
