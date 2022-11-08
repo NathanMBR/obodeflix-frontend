1c7fbe4743fbc868c40ae5f352696811e23fdd40
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2022-11-08 11:36:52 -0300

    chore: allow styling at default header component

--- src/components/DefaultHeader/DefaultHeader.tsx
+++ src/components/DefaultHeader/DefaultHeader.tsx
@@ -2,18 +2,26 @@ import {
     Divider,
     Typography
 } from "@mui/material";
+import { CSSProperties } from "react";
 
 export interface DefaultHeaderProps {
     children: string;
+    style?: CSSProperties;
 }
 
 export const DefaultHeader = (props: DefaultHeaderProps) => {
-    const { children } = props;
+    const {
+        children,
+        style
+    } = props;
 
     return (
-        <>
+        <div style={{
+            marginBottom: 16,
+            ...style
+        }}>
             <Typography variant="h4" component="h2">{children}</Typography>
             <Divider />
-        </>
+        </div>
     );
 }
