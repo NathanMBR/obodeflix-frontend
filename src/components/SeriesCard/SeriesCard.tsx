b803e10033c7c08af67588a017717fd8f4a49736
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2022-11-22 20:07:11 -0300

    fix: correctly define image size at series card component

--- src/components/SeriesCard/SeriesCard.tsx
+++ src/components/SeriesCard/SeriesCard.tsx
@@ -6,8 +6,11 @@ import {
     Divider,
     Grid,
     Paper,
+    Stack,
     Typography
 } from "@mui/material";
+import { ImageNotSupported } from "@mui/icons-material";
+import { CSSProperties } from "@mui/styled-engine";
 
 import { Series } from "../../types";
 
@@ -22,17 +25,68 @@ export const SeriesCard = (props: SeriesCardProps) => {
         imageAddress
     } = props.series;
 
+    const imageStyle: CSSProperties = {
+        width: "220px",
+        height: "320px"
+    };
+
+    const noImageStyle: CSSProperties = {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+
+        width: "220px",
+        height: "320px"
+    };
+
     return (
         <>
-            <Grid item xs={2.4} style={{ display: "flex" }}>
-                <Paper elevation={12} style={{ display: "flex" }}>
+            <Grid item
+                xs={2.4}
+                style={{ display: "flex" }}
+            >
+                <Paper
+                    elevation={12}
+                    style={{ display: "flex" }}
+                >
                     <Card>
-                        <CardActionArea style={{ height: "100%" }} LinkComponent="a" href={`/series/${id}`}>
-                            <CardMedia component="img" image={imageAddress || "/no-image"}>
-                            </CardMedia>
+                        <CardActionArea
+                            style={{ height: "100%" }}
+                            LinkComponent="a"
+                            href={`/series/${id}`}
+                        >
+
+                            {
+                                imageAddress
+                                    ? <CardMedia
+                                        component="img"
+                                        src={imageAddress || ""}
+                                        sx={imageStyle}
+                                    />
+                                    : <>
+                                        <Stack
+                                            direction="column"
+                                            sx={noImageStyle}
+                                        >
+                                            <ImageNotSupported />
+                                            <Typography
+                                                variant="subtitle2"
+                                            >
+                                                Imagem indisponível
+                                            </Typography>
+                                        </Stack>
+                                    </>
+                            }
+
                             <Divider />
                             <CardContent>
-                                <Typography variant="subtitle2" component="h3" textAlign="center">{mainName}</Typography>
+                                <Typography
+                                    variant="subtitle2"
+                                    component="h3"
+                                    textAlign="center"
+                                >
+                                    {mainName}
+                                </Typography>
                             </CardContent>
                         </CardActionArea>
                     </Card>
