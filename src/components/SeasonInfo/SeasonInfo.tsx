25fab31869b99028228d988d52a8ae76e2c6264f
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2023-03-01 14:57:37 -0300

    feat: show image at season info component

--- src/components/SeasonInfo/SeasonInfo.tsx
+++ src/components/SeasonInfo/SeasonInfo.tsx
@@ -1,12 +1,16 @@
 import {
     Box,
+    CardMedia,
     Divider,
     IconButton,
     Stack,
     Tooltip,
     Typography
 } from "@mui/material";
-import { Visibility } from "@mui/icons-material";
+import {
+    ImageNotSupported,
+    Visibility
+} from "@mui/icons-material";
 import { CSSProperties } from "@mui/styled-engine";
 import { Link } from "react-router-dom";
 
@@ -28,38 +32,92 @@ export const SeasonInfo = (props: SeasonInfoProps) => {
 
     if (!season)
         return null;
+
+    const imageStyle: CSSProperties = {
+        border: "1px solid black",
+        borderRadius: "5%",
+        maxWidth: "200px",
+        maxHeight: "280px",
+        float: "left",
+        marginRight: 2,
+        marginBottom: 2
+    };
+
+    const noImageStyle: CSSProperties = {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        border: "1px solid black",
+        borderRadius: "5%",
+        width: "100vw",
+        height: "100vh",
+        maxWidth: "200px",
+        maxHeight: "280px",
+        float: "left",
+        marginRight: 2
+    };
     
     return (
         <Box sx={sx}>
-            <Stack
-                direction="row"
-                sx={{ marginBottom: 2 }}
-            >
-                <Typography
-                    variant="h4"
-                    component="h2"
-                >
-                    {season.name}
-                </Typography>
+            <Stack direction="column">
+                <Box>
+                    {
+                        season.imageAddress
+                            ? <>
+                                <CardMedia
+                                    component="img"
+                                    src={season.imageAddress || ""}
+                                    sx={imageStyle}
+                                />
+                            </>
+                            : <>
+                                <Stack
+                                    direction="column"
+                                    sx={noImageStyle}
+                                >
+                                    <ImageNotSupported
+                                        sx={{ fontSize: 32 }}
+                                    />
+                                    <Typography variant="subtitle2">
+                                        Imagem indisponível
+                                    </Typography>
+                                </Stack>
+                            </>
+                    }
 
-                <Tooltip title="Ver série relacionada">
-                    <Link to={`/series/${season.seriesId}`}>
-                        <IconButton>
-                            <Visibility />
-                        </IconButton>
-                    </Link>
-                </Tooltip>
+                    <Box>
+                        <Stack
+                            direction="row"
+                            sx={{ marginBottom: 2 }}
+                        >
+                            <Typography
+                                variant="h4"
+                                component="h2"
+                            >
+                                {season.name}
+                            </Typography>
+
+                            <Tooltip title="Ver série relacionada">
+                                <Link to={`/series/${season.seriesId}`}>
+                                    <IconButton>
+                                        <Visibility />
+                                    </IconButton>
+                                </Link>
+                            </Tooltip>
+                        </Stack>
+
+                        <Divider style={{ marginBottom: 16 }} />
+
+                        <Typography variant="body1">
+                            <b>Tipo:</b> {SeasonTypesEnum[season.type]}
+                        </Typography>
+                        
+                        <Typography variant="body1">
+                            <b>Posição cronológica:</b> {season.position}
+                        </Typography>
+                    </Box>
+                </Box>
             </Stack>
-
-            <Divider style={{ marginBottom: 16 }} />
-
-            <Typography variant="body1">
-                <b>Tipo:</b> {SeasonTypesEnum[season.type]}
-            </Typography>
-            
-            <Typography variant="body1">
-                <b>Posição cronológica:</b> {season.position}
-            </Typography>
         </Box>
     );
 };
