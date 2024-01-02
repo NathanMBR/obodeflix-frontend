6a57c093fd90ac438783087b6feaa8c2de6898b0
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2024-01-02 18:46:31 -0300

    feat: add unused folders option to admin panel page

--- src/pages/AdminPanel.tsx
+++ src/pages/AdminPanel.tsx
@@ -1,75 +1,80 @@
 import {
-    Divider,
-    Grid,
-    Typography
+  Divider,
+  Grid,
+  Typography
 } from "@mui/material";
 
 import { NotFound } from "../pages";
 import { AdminPanelOption } from "../components";
 
 export const AdminPanel = () => {
-    const hasPermissionToAccess = localStorage.getItem("token") && localStorage.getItem("type") === "ADMIN";
-    if (!hasPermissionToAccess)
-        return <NotFound />;
+  const hasPermissionToAccess = localStorage.getItem("token") && localStorage.getItem("type") === "ADMIN";
+  if (!hasPermissionToAccess)
+    return <NotFound />;
 
-    interface PanelOption {
-        title: string;
-        link: string;
+  interface PanelOption {
+    title: string;
+    link: string;
+  }
+  const panelOptions: Array<PanelOption> = [
+    {
+      title: "Séries",
+      link: "/admin/series"
+    },
+
+    {
+      title: "Tags",
+      link: "/admin/tags"
+    },
+
+    {
+      title: "Temporadas",
+      link: "/admin/seasons"
+    },
+
+    {
+      title: "Episódios",
+      link: "/admin/episodes"
+    },
+
+    {
+      title: "Importação de Episódios",
+      link: "/admin/episodes-import"
+    },
+
+    {
+      title: "Pastas não importadas",
+      link: "/admin/unused-folders"
     }
-    const panelOptions: Array<PanelOption> = [
-        {
-            title: "Séries",
-            link: "/admin/series"
-        },
+  ];
 
+  return (
+    <>
+      <Typography
+        variant="h4"
+        component="h2"
+      >
+        Painel administrativo
+      </Typography>
+      <Divider />
+      <Grid container
+        spacing={2}
+        style={{ marginTop: 2 }}
+      >
         {
-            title: "Tags",
-            link: "/admin/tags"
-        },
-
-        {
-            title: "Temporadas",
-            link: "/admin/seasons"
-        },
-
-        {
-            title: "Episódios",
-            link: "/admin/episodes"
-        },
-
-        {
-            title: "Importação de Episódios",
-            link: "/admin/episodes-import"
-        }
-    ];
-
-    return (
-        <>
-            <Typography
-                variant="h4"
-                component="h2"
+          panelOptions.map(
+            (options, index) => <Grid item
+              key={index}
+              xs={index === panelOptions.length - 1 && panelOptions.length % 2 === 1 ? 12 : 6}
             >
-                Painel administrativo
-            </Typography>
-            <Divider />
-            <Grid container
-                spacing={2}
-                style={{ marginTop: 2 }}
-            >
-                {
-                    panelOptions.map(
-                        (options, index) => <Grid item
-                            key={index}
-                            xs={index === panelOptions.length - 1 ? 12 : 6}
-                        >
-                            <AdminPanelOption
-                                title={options.title}
-                                link={options.link}
-                            />
-                        </Grid>
-                    )
-                }
+              <AdminPanelOption
+                title={options.title}
+                link={options.link}
+              />
             </Grid>
-        </>
-    )
+          )
+        }
+      </Grid>
+    </>
+  )
 }
