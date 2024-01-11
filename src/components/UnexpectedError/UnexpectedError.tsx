442763ac7793d611f0674ace3fe1e40873b79e50
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2024-01-11 20:18:47 -0300

    chore: add unexpected error component

---
+++ src/components/UnexpectedError/UnexpectedError.tsx
@@ -0,0 +1,11 @@
+import { Typography } from "@mui/material"
+
+export const UnexpectedError = () => {
+  return (
+    <>
+      <Typography variant="body1">
+        Um erro inesperado ocorreu.
+      </Typography>
+    </>
+  )
+}
