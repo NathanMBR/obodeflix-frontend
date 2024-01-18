9b5c883052bb59516d2c0a0a0ce88914d1bf0d13
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2024-01-17 21:34:04 -0300

    style: clear old code style

--- src/pages/OneSeason.tsx
+++ src/pages/OneSeason.tsx
@@ -1,253 +1,251 @@
 import {
-    Box,
-    CircularProgress,
-    Grid,
-    SelectChangeEvent,
-    Typography
-} from "@mui/material";
-import { CSSProperties } from "@mui/styled-engine";
+  Box,
+  CircularProgress,
+  Grid,
+  type SelectChangeEvent,
+  Typography
+} from "@mui/material"
 import {
-    ChangeEvent,
-    useEffect,
-    useState
-} from "react";
-import { useParams } from "react-router-dom";
+  type ChangeEvent,
+  type CSSProperties,
+  useEffect,
+  useState
+} from "react"
+import { useParams } from "react-router-dom"
 
 import {
-    EpisodeCard,
-    ErrorCard,
-    ErrorCardStatusCodeProp,
-    SeasonInfo
-} from "../components";
+  EpisodeCard,
+  ErrorCard,
+  type ErrorCardStatusCodeProp,
+  SeasonInfo
+} from "../components"
 import {
-    Season,
-    SeasonBuilder,
-    Episode,
-    Pagination,
-    PaginationBuilder,
-    EpisodeOrderColumn,
-    OrderBy
-} from "../types";
-import { PaginatedContent } from "../layouts";
-import { NotFound } from "../pages";
-import { API_URL } from "../settings";
+  type Season,
+  SeasonBuilder,
+  type Episode,
+  type Pagination,
+  PaginationBuilder,
+  type EpisodeOrderColumn,
+  type OrderBy
+} from "../types"
+import { PaginatedContent } from "../layouts"
+import { NotFound } from "../pages"
+import { API_URL } from "../settings"
 
-export type OneSeasonParams = Record<"id", string>;
+export type OneSeasonParams = Record<"id", string>
 
 export const OneSeason = () => {
-    const seasonId = Number(useParams<OneSeasonParams>().id);
+  const seasonId = Number(useParams<OneSeasonParams>().id)
 
-    if (Number.isNaN(seasonId) || seasonId <= 0)
-        return <NotFound />;
+  if (Number.isNaN(seasonId) || seasonId <= 0)
+    return <NotFound />
 
-    const [season, setSeason] = useState<Season | undefined>(undefined);
+  const [season, setSeason] = useState<Season | undefined>(undefined)
 
-    const [isRequestLoading, setIsRequestLoading] = useState(false);
-    const [statusCode, setStatusCode] = useState<ErrorCardStatusCodeProp>(null);
-    const [reasons, setReasons] = useState<string | Array<string>>();
+  const [isRequestLoading, setIsRequestLoading] = useState(false)
+  const [statusCode, setStatusCode] = useState<ErrorCardStatusCodeProp>(null)
+  const [reasons, setReasons] = useState<string | Array<string>>()
 
-    const [paginatedSeasonEpisodes, setPaginatedSeasonEpisodes] = useState<Pagination<Episode> | null>(null);
-    const [episodes, setEpisodes] = useState<Array<Episode>>([]);
-    const [isEpisodesLoading, setIsEpisodesLoading] = useState(false);
-    const [page, setPage] = useState(1);
-    const [quantity, setQuantity] = useState(50);
-    const [orderColumn, setOrderColumn] = useState<EpisodeOrderColumn>("position");
-    const [orderBy, setOrderBy] = useState<OrderBy>("asc");
-    const [search, setSearch] = useState("");
+  const [paginatedSeasonEpisodes, setPaginatedSeasonEpisodes] = useState<Pagination<Episode> | null>(null)
+  const [episodes, setEpisodes] = useState<Array<Episode>>([])
+  const [isEpisodesLoading, setIsEpisodesLoading] = useState(false)
+  const [page, setPage] = useState(1)
+  const [quantity, setQuantity] = useState(50)
+  const [orderColumn, setOrderColumn] = useState<EpisodeOrderColumn>("position")
+  const [orderBy, setOrderBy] = useState<OrderBy>("asc")
+  const [search, setSearch] = useState("")
 
-    const handleSeasonFetchResponse = async (response: Response) => {
-        const data = await response.json();
+  const handleSeasonFetchResponse = async (response: Response) => {
+    const data = await response.json()
 
-        if (!response.ok) {
-            setStatusCode(response.status as ErrorCardStatusCodeProp);
+    if (!response.ok) {
+      setStatusCode(response.status as ErrorCardStatusCodeProp)
 
-            if (data.reason)
-                setReasons(data.reason);
+      if (data.reason)
+        setReasons(data.reason)
 
-            return;
-        }
+      return
+    }
 
-        const builtSeason = new SeasonBuilder(data);
-        setSeason(builtSeason);
-    };
+    const builtSeason = new SeasonBuilder(data)
+    setSeason(builtSeason)
+  }
 
-    const handleEpisodesFetchResponse = async (response: Response) => {
-        const data = await response.json();
+  const handleEpisodesFetchResponse = async (response: Response) => {
+    const data = await response.json()
 
-        if (!response.ok) {
-            setStatusCode(response.status as ErrorCardStatusCodeProp);
+    if (!response.ok) {
+      setStatusCode(response.status as ErrorCardStatusCodeProp)
 
-            if (data.reason)
-                setReasons(data.reason);
+      if (data.reason)
+        setReasons(data.reason)
 
-            return;
-        }
+      return
+    }
 
-        const builtPaginatedSeasonEpisodes = new PaginationBuilder<Episode>(data);
-        setPaginatedSeasonEpisodes(builtPaginatedSeasonEpisodes);
-    };
+    const builtPaginatedSeasonEpisodes = new PaginationBuilder<Episode>(data)
+    setPaginatedSeasonEpisodes(builtPaginatedSeasonEpisodes)
+  }
 
-    const handlePageChange = (_event: ChangeEvent<unknown>, page: number) => {
-        setPage(page);
-    };
+  const handlePageChange = (_event: ChangeEvent<unknown>, page: number) => {
+    setPage(page)
+  }
 
-    const handleOrderColumnChange = (event: SelectChangeEvent<EpisodeOrderColumn>) => {
-        setOrderColumn(event.target.value as EpisodeOrderColumn);
-    };
+  const handleOrderColumnChange = (event: SelectChangeEvent<EpisodeOrderColumn>) => {
+    setOrderColumn(event.target.value as EpisodeOrderColumn)
+  }
 
-    const handleOrderByChange = (event: SelectChangeEvent<OrderBy>) => {
-        setOrderBy(event.target.value as OrderBy);
-    };
+  const handleOrderByChange = (event: SelectChangeEvent<OrderBy>) => {
+    setOrderBy(event.target.value as OrderBy)
+  }
 
-    const handleQuantityChange = (event: SelectChangeEvent<number>) => {
-        const newQuantity = Number(event.target.value);
+  const handleQuantityChange = (event: SelectChangeEvent<number>) => {
+    const newQuantity = Number(event.target.value)
 
-        if (Number.isNaN(newQuantity))
-            return;
+    if (Number.isNaN(newQuantity))
+      return
 
-        setQuantity(newQuantity);
-    };
+    setQuantity(newQuantity)
+  }
 
-    const handleSearchChange = (newSearch: string) => {
-        setSearch(newSearch);
-    };
+  const handleSearchChange = (newSearch: string) => {
+    setSearch(newSearch)
+  }
 
-    const handleErrorCardClose = () => {
-        setStatusCode(null);
-        setReasons(undefined);
-        history.back();
-    };
+  const handleErrorCardClose = () => {
+    setStatusCode(null)
+    setReasons(undefined)
+    history.back()
+  }
 
-    useEffect(
-        () => {
-            setIsRequestLoading(true);
+  useEffect(
+    () => {
+      setIsRequestLoading(true)
 
-            fetch(`${API_URL}/season/get/${seasonId}`)
-                .then(handleSeasonFetchResponse)
-                .catch(console.error)
-                .finally(() => setIsRequestLoading(false));
-        },
+      fetch(`${API_URL}/season/get/${seasonId}`)
+        .then(handleSeasonFetchResponse)
+        .catch(console.error)
+        .finally(() => setIsRequestLoading(false))
+    },
 
-        []
-    );
+    []
+  )
 
-    useEffect(
-        () => {
-            if (!season)
-                return;
+  useEffect(
+    () => {
+      if (!season)
+        return
 
-            setIsEpisodesLoading(true);
+      setIsEpisodesLoading(true)
 
-            fetch(`${API_URL}/episode/all?seasonId=${season.id}&page=${page}&quantity=${quantity}&orderColumn=${orderColumn}&orderBy=${orderBy}${search ? `&search=${search}` : ""}`)
-                .then(handleEpisodesFetchResponse)
-                .catch(console.error)
-                .finally(() => setIsEpisodesLoading(false));
-        },
+      fetch(`${API_URL}/episode/all?seasonId=${season.id}&page=${page}&quantity=${quantity}&orderColumn=${orderColumn}&orderBy=${orderBy}${search ? `&search=${search}` : ""}`)
+        .then(handleEpisodesFetchResponse)
+        .catch(console.error)
+        .finally(() => setIsEpisodesLoading(false))
+    },
 
-        [
-            season,
-            page,
-            quantity,
-            orderColumn,
-            orderBy,
-            search
-        ]
-    );
+    [
+      season,
+      page,
+      quantity,
+      orderColumn,
+      orderBy,
+      search
+    ]
+  )
 
-    useEffect(
-        () => {
-            paginatedSeasonEpisodes
-                ? setEpisodes(paginatedSeasonEpisodes.data)
-                : setEpisodes([]);
+  useEffect(
+    () => {
+      paginatedSeasonEpisodes
+        ? setEpisodes(paginatedSeasonEpisodes.data)
+        : setEpisodes([])
 
-            const isCurrentPageEmpty = paginatedSeasonEpisodes &&
-                paginatedSeasonEpisodes.data.length <= 0 &&
-                paginatedSeasonEpisodes.currentPage > 1;
+      const isCurrentPageEmpty = paginatedSeasonEpisodes &&
+        paginatedSeasonEpisodes.data.length <= 0 &&
+        paginatedSeasonEpisodes.currentPage > 1
 
-            if (isCurrentPageEmpty)
-                setPage(1);
-        },
+      if (isCurrentPageEmpty)
+        setPage(1)
+    },
 
-        [paginatedSeasonEpisodes]
-    )
+    [paginatedSeasonEpisodes]
+  )
 
-    const loadingStyle: CSSProperties = {
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center"
-    };
+  const loadingStyle: CSSProperties = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center"
+  }
 
-    const noEpisodeMessage = <>
-        <Typography variant="body1">Não há episódios a serem exibidos.</Typography>
-    </>;
+  const noEpisodeMessage = <>
+    <Typography variant="body1">Não há episódios a serem exibidos.</Typography>
+  </>
 
-    return (
-        <>
-            {
-                isRequestLoading
-                    ? <Box sx={loadingStyle}>
-                        <CircularProgress />
-                    </Box>
-                    : <>
-                        <SeasonInfo
-                            season={season}
-                            sx={{ marginBottom: 4 }}
-                        />
-
-                        <PaginatedContent<EpisodeOrderColumn>
-                            contentTitle="Episódios"
-                            hidePaginationContent={episodes.length <= 0}
-                            isRequestLoading={isEpisodesLoading}
-                            currentQuantity={episodes.length}
-                            totalQuantity={paginatedSeasonEpisodes?.totalQuantity || 0}
-                            noContent={noEpisodeMessage}
-
-                            newSearch={search}
-                            handleSearchChange={handleSearchChange}
-
-                            quantityPerPage={quantity}
-                            handleQuantityPerPageChange={handleQuantityChange}
-
-                            page={page}
-                            handlePageChange={handlePageChange}
-                            lastPage={paginatedSeasonEpisodes?.lastPage || 1}
-
-                            orderBy={orderBy}
-                            handleOrderByChange={handleOrderByChange}
-
-                            orderColumns={
-                                [
-                                    ["id", "ID"],
-                                    ["name", "Nome"],
-                                    ["position", "Ordem cronológica"],
-                                    ["updatedAt", "Recentemente atualizado"],
-                                ]
-                            }
-                            handleOrderColumnChange={handleOrderColumnChange}
-                            currentOrderColumn={orderColumn}
-                        >
-                            <Grid container
-                                spacing={2}
-                            >
-                                {
-                                    episodes.map(
-                                        (episode, index) => <EpisodeCard
-                                            key={index}
-                                            episode={episode}
-                                        />
-                                    )
-                                }
-                            </Grid>
-                        </PaginatedContent>
-                    </>
-            }
-
-            <ErrorCard
-                isOpen={!!statusCode}
-                statusCode={statusCode}
-                reasons={reasons}
-                handleClose={handleErrorCardClose}
+  return (
+    <>
+      {
+        isRequestLoading
+          ? <Box sx={loadingStyle}>
+            <CircularProgress />
+          </Box>
+          : <>
+            <SeasonInfo
+              season={season}
+              sx={{ marginBottom: 4 }}
             />
-        </>
-    );
-};
+
+            <PaginatedContent<EpisodeOrderColumn>
+              contentTitle="Episódios"
+              hidePaginationContent={episodes.length <= 0}
+              isRequestLoading={isEpisodesLoading}
+              currentQuantity={episodes.length}
+              totalQuantity={paginatedSeasonEpisodes?.totalQuantity || 0}
+              noContent={noEpisodeMessage}
+
+              newSearch={search}
+              handleSearchChange={handleSearchChange}
+
+              quantityPerPage={quantity}
+              handleQuantityPerPageChange={handleQuantityChange}
+
+              page={page}
+              handlePageChange={handlePageChange}
+              lastPage={paginatedSeasonEpisodes?.lastPage || 1}
+
+              orderBy={orderBy}
+              handleOrderByChange={handleOrderByChange}
+
+              orderColumns={
+                [
+                  ["id", "ID"],
+                  ["name", "Nome"],
+                  ["position", "Ordem cronológica"],
+                  ["updatedAt", "Recentemente atualizado"],
+                ]
+              }
+              handleOrderColumnChange={handleOrderColumnChange}
+              currentOrderColumn={orderColumn}
+            >
+              <Grid container spacing={2}>
+                {
+                  episodes.map(
+                    (episode, index) => <EpisodeCard
+                      key={index}
+                      episode={episode}
+                    />
+                  )
+                }
+              </Grid>
+            </PaginatedContent>
+          </>
+      }
+
+      <ErrorCard
+        isOpen={!!statusCode}
+        statusCode={statusCode}
+        reasons={reasons}
+        handleClose={handleErrorCardClose}
+      />
+    </>
+  )
+}
