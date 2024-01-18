d098261bc0af3c517d3701286b81cb2078d67fe2
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2024-01-17 21:27:41 -0300

    chore: remove position info from season info card

--- src/components/SeasonInfo/SeasonInfo.tsx
+++ src/components/SeasonInfo/SeasonInfo.tsx
@@ -1,127 +1,123 @@
 import {
-    Box,
-    CardMedia,
-    Divider,
-    IconButton,
-    Stack,
-    Tooltip,
-    Typography
-} from "@mui/material";
+  Box,
+  CardMedia,
+  Divider,
+  IconButton,
+  Stack,
+  Tooltip,
+  Typography
+} from "@mui/material"
 import {
-    ImageNotSupported,
-    Visibility
-} from "@mui/icons-material";
-import { CSSProperties } from "@mui/styled-engine";
-import { Link } from "react-router-dom";
+  ImageNotSupported,
+  Visibility
+} from "@mui/icons-material"
+import { Link } from "react-router-dom"
+import type { CSSProperties } from "react"
 
 import {
-    Season,
-    SeasonTypesEnum
-} from "../../types";
+  Season,
+  SeasonTypesEnum
+} from "../../types"
 
 export interface SeasonInfoProps {
-    season?: Season;
-    sx?: CSSProperties;
-};
+  season?: Season
+  sx?: CSSProperties
+}
 
 export const SeasonInfo = (props: SeasonInfoProps) => {
-    const {
-        season,
-        sx
-    } = props;
+  const {
+    season,
+    sx
+  } = props
 
-    if (!season)
-        return null;
+  if (!season)
+    return null
 
-    const imageStyle: CSSProperties = {
-        border: "1px solid black",
-        borderRadius: "5%",
-        maxWidth: "200px",
-        maxHeight: "280px",
-        float: "left",
-        marginRight: 2,
-        marginBottom: 2
-    };
+  const imageStyle: CSSProperties = {
+    border: "1px solid black",
+    borderRadius: "5%",
+    maxWidth: "200px",
+    maxHeight: "280px",
+    float: "left",
+    marginRight: 2,
+    marginBottom: 2
+  }
 
-    const noImageStyle: CSSProperties = {
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        border: "1px solid black",
-        borderRadius: "5%",
-        width: "100vw",
-        height: "100vh",
-        maxWidth: "200px",
-        maxHeight: "280px",
-        float: "left",
-        marginRight: 2
-    };
-    
-    return (
-        <Box sx={sx}>
-            <Stack direction="column">
-                <Box>
-                    {
-                        season.imageAddress
-                            ? <>
-                                <CardMedia
-                                    component="img"
-                                    src={season.imageAddress || ""}
-                                    sx={imageStyle}
-                                />
-                            </>
-                            : <>
-                                <Stack
-                                    direction="column"
-                                    sx={noImageStyle}
-                                >
-                                    <ImageNotSupported
-                                        sx={{ fontSize: 32 }}
-                                    />
-                                    <Typography variant="subtitle2">
-                                        Imagem indisponível
-                                    </Typography>
-                                </Stack>
-                            </>
-                    }
+  const noImageStyle: CSSProperties = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    border: "1px solid black",
+    borderRadius: "5%",
+    width: "100vw",
+    height: "100vh",
+    maxWidth: "200px",
+    maxHeight: "280px",
+    float: "left",
+    marginRight: 2
+  }
 
-                    <Box>
-                        <Stack
-                            direction="row"
-                            sx={{ marginBottom: 2 }}
-                        >
-                            <Typography
-                                variant="h4"
-                                component="h2"
-                            >
-                                {season.name}
-                            </Typography>
+  return (
+    <Box sx={sx}>
+      <Stack direction="column">
+        <Box>
+          {
+            season.imageAddress
+              ? <>
+                <CardMedia
+                  component="img"
+                  src={season.imageAddress || ""}
+                  sx={imageStyle}
+                />
+              </>
+              : <>
+                <Stack
+                  direction="column"
+                  sx={noImageStyle}
+                >
+                  <ImageNotSupported
+                    sx={{ fontSize: 32 }}
+                  />
+                  <Typography variant="subtitle2">
+                    Imagem indisponível
+                  </Typography>
+                </Stack>
+              </>
+          }
 
-                            <Tooltip title="Ver série relacionada">
-                                <Link to={`/series/${season.seriesId}`}>
-                                    <IconButton>
-                                        <Visibility />
-                                    </IconButton>
-                                </Link>
-                            </Tooltip>
-                        </Stack>
+          <Box>
+            <Stack
+              direction="row"
+              sx={{ marginBottom: 2 }}
+            >
+              <Typography
+                variant="h4"
+                component="h2"
+              >
+                {season.name}
+              </Typography>
 
-                        <Divider style={{ marginBottom: 16 }} />
-
-                        <Typography variant="body1">
-                            <b>Tipo:</b> {SeasonTypesEnum[season.type]}
-                        </Typography>
-                        
-                        <Typography variant="body1">
-                            <b>Posição cronológica:</b> {season.position}
-                        </Typography>
-
-                        <Typography variant="body1">
-                            <b>Descrição:</b> {season.description ? season.description : <i>(Vazio)</i>}
-                        </Typography>
-                    </Box>
-                </Box>
+              <Tooltip title="Ver série relacionada">
+                <Link to={`/series/${season.seriesId}`}>
+                  <IconButton>
+                    <Visibility />
+                  </IconButton>
+                </Link>
+              </Tooltip>
             </Stack>
+
+            <Divider style={{ marginBottom: 16 }} />
+
+            <Typography variant="body1">
+              <b>Tipo:</b> {SeasonTypesEnum[season.type]}
+            </Typography>
+
+            <Typography variant="body1">
+              <b>Descrição:</b> {season.description ? season.description : <i>(Vazio)</i>}
+            </Typography>
+          </Box>
         </Box>
-    );
-};
+      </Stack>
+    </Box>
+  )
+}
