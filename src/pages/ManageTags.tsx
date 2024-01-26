079b775b9d0abef4387834608ca6ec8364294323
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2024-01-26 09:12:14 -0300

    feat: order by name at manage tags page

--- src/pages/ManageTags.tsx
+++ src/pages/ManageTags.tsx
@@ -1,237 +1,257 @@
 import {
-    Box,
-    SelectChangeEvent,
-    Typography
-} from "@mui/material";
+  Box,
+  type SelectChangeEvent,
+  Typography
+} from "@mui/material"
 import {
-    ChangeEvent,
-    useState,
-    useEffect
-} from "react";
+  type ChangeEvent,
+  useState,
+  useEffect
+} from "react"
 
-import { NotFound } from "../pages";
-import { PaginatedContent } from "../layouts";
 import {
-    AdminPanelAddContentFAB,
-    AdminPanelDeleteTagCard,
-    AdminPanelTagTable,
-    ErrorCard,
-    ErrorCardStatusCodeProp,
-    SuccessCard
-} from "../components";
+  AdminPanelAddContentFAB,
+  AdminPanelDeleteTagCard,
+  AdminPanelTagTable,
+  ErrorCard,
+  type ErrorCardStatusCodeProp,
+  SuccessCard
+} from "../components"
 import {
-    OrderBy,
-    Pagination,
-    PaginationBuilder,
-    Tag,
-    TagOrderColumn
-} from "../types";
-import { API_URL } from "../settings";
+  type OrderBy,
+  type Pagination,
+  PaginationBuilder,
+  type Tag,
+  type TagOrderColumn
+} from "../types"
+import { NotFound } from "../pages"
+import { PaginatedContent } from "../layouts"
+import { API_URL } from "../settings"
 
 export const ManageTags = () => {
-    const hasPermissionToAccess = localStorage.getItem("token") && localStorage.getItem("type") === "ADMIN";
-    if (!hasPermissionToAccess)
-        return <NotFound />;
+  const userToken = localStorage.getItem("token")
+  const hasPermissionToAccess = !!userToken && localStorage.getItem("type") === "ADMIN"
+  if (!hasPermissionToAccess)
+    return <NotFound />
 
-    const [isRequestLoading, setIsRequestLoading] = useState(false);
-    const [statusCode, setStatusCode] = useState<ErrorCardStatusCodeProp>(null);
+  const [isRequestLoading, setIsRequestLoading] = useState(false)
+  const [statusCode, setStatusCode] = useState<ErrorCardStatusCodeProp>(null)
 
-    const [page, setPage] = useState(1);
-    const [quantity, setQuantity] = useState(50);
-    const [orderColumn, setOrderColumn] = useState<TagOrderColumn>("id");
-    const [orderBy, setOrderBy] = useState<OrderBy>("asc");
-    const [search, setSearch] = useState("");
+  const [page, setPage] = useState(1)
+  const [quantity, setQuantity] = useState(50)
+  const [orderColumn, setOrderColumn] = useState<TagOrderColumn>("name")
+  const [orderBy, setOrderBy] = useState<OrderBy>("asc")
+  const [search, setSearch] = useState("")
 
-    const [paginatedTags, setPaginatedTags] = useState<Pagination<Tag> | null>(null);
-    const [tags, setTags] = useState<Array<Tag>>([]);
+  const [paginatedTags, setPaginatedTags] = useState<Pagination<Tag> | null>(null)
+  const [tags, setTags] = useState<Array<Tag>>([])
 
-    const [deleteTagCardData, setDeleteTagCardData] = useState<Tag | null>(null);
-    const [showSuccessCard, setShowSuccessCard] = useState(false);
+  const [deleteTagCardData, setDeleteTagCardData] = useState<Tag | null>(null)
+  const [showSuccessCard, setShowSuccessCard] = useState(false)
 
-    const handlePageChange = (_event: ChangeEvent<unknown>, page: number) => {
-        setPage(page);
+  const handlePageChange = (_event: ChangeEvent<unknown>, page: number) => {
+    setPage(page)
+  }
+
+  const handleOrderColumnChange = (event: SelectChangeEvent<TagOrderColumn>) => {
+    setOrderColumn(event.target.value as TagOrderColumn)
+  }
+
+  const handleOrderByChange = (event: SelectChangeEvent<OrderBy>) => {
+    setOrderBy(event.target.value as OrderBy)
+  }
+
+  const handleQuantityChange = (event: SelectChangeEvent<number>) => {
+    const newQuantity = Number(event.target.value)
+
+    if (Number.isNaN(newQuantity))
+      return
+
+    setQuantity(newQuantity)
+  }
+
+  const handleErrorClose = () => {
+    setStatusCode(null)
+    window.location.href = "/"
+  }
+
+  const handleFetchAllTagsResponse = async (response: Response) => {
+    if (!response.ok)
+      return setStatusCode(response.status as ErrorCardStatusCodeProp)
+
+    const data = await response.json()
+    const builtPaginatedTags = new PaginationBuilder<Tag>(data)
+    setPaginatedTags(builtPaginatedTags)
+  }
+
+  const fetchAllTags = async () => {
+    try {
+      const url = new URL(`${API_URL}/tag/all`, window.location.origin)
+      url.searchParams.append("page", String(page))
+      url.searchParams.append("quantity", String(quantity))
+      url.searchParams.append("orderColumn", String(orderColumn))
+      url.searchParams.append("orderBy", String(orderBy))
+
+      if (search)
+        url.searchParams.append("search", search)
+
+      const urlString = url.toString()
+      const response = await fetch(urlString)
+      await handleFetchAllTagsResponse(response)
+    } catch (error) {
+      console.error(error)
+    } finally {
+      setIsRequestLoading(false)
+    }
+  }
+
+  const handleDeleteTagResponse = async (response: Response) => {
+    if (!response.ok)
+      return setStatusCode(response.status as ErrorCardStatusCodeProp)
+
+    setShowSuccessCard(true)
+  }
+
+  const getOpenDeleteTagCardHandler = (tag: Tag) => {
+    const handleOpenDeleteTagCard = () => {
+      setDeleteTagCardData(tag)
     }
 
-    const handleOrderColumnChange = (event: SelectChangeEvent<TagOrderColumn>) => {
-        setOrderColumn(event.target.value as TagOrderColumn);
-    };
+    return handleOpenDeleteTagCard
+  }
 
-    const handleOrderByChange = (event: SelectChangeEvent<OrderBy>) => {
-        setOrderBy(event.target.value as OrderBy);
-    };
+  const getDeleteTagHandler = (tag: Tag | null) => {
+    const finishDeleteProcess = () => {
+      setDeleteTagCardData(null)
 
-    const handleQuantityChange = (event: SelectChangeEvent<number>) => {
-        const newQuantity = Number(event.target.value);
+      fetchAllTags()
+    }
 
-        if (Number.isNaN(newQuantity))
-            return;
+    return () => {
+      if (!tag)
+        return
 
-        setQuantity(newQuantity);
-    };
+      const url = new URL(`${API_URL}/tag/inactivate/${tag.id}`, window.location.origin)
+      const urlString = url.toString()
 
-    const handleErrorClose = () => {
-        setStatusCode(null);
-        window.location.href = "/";
-    };
-
-    const handleFetchAllTagsResponse = async (response: Response) => {
-        if (!response.ok)
-            return setStatusCode(response.status as ErrorCardStatusCodeProp);
-
-        const data = await response.json();
-        const builtPaginatedTags = new PaginationBuilder<Tag>(data);
-        setPaginatedTags(builtPaginatedTags);
-    };
-
-    const fetchAllTags = async () => fetch(`${API_URL}/tag/all?page=${page}&quantity=${quantity}&orderColumn=${orderColumn}&orderBy=${orderBy}${search.length > 0 ? `&search=${search}` : ""}`)
-        .then(handleFetchAllTagsResponse)
+      fetch(
+        urlString,
+        {
+          method: "DELETE",
+          headers: {
+            Authorization: `Bearer ${userToken}`
+          }
+        }
+      )
+        .then(handleDeleteTagResponse)
         .catch(console.error)
-        .finally(() => setIsRequestLoading(false));
+        .finally(finishDeleteProcess)
+    }
+  }
 
-    const handleDeleteTagResponse = async (response: Response) => {
-        if (!response.ok)
-            return setStatusCode(response.status as ErrorCardStatusCodeProp);
+  const handleDeleteTagCardClose = () => {
+    setDeleteTagCardData(null)
+  }
 
-        setShowSuccessCard(true);
-    };
+  const handleSearchChange = (newSearch: string) => {
+    setSearch(newSearch)
+  }
 
-    const getOpenDeleteTagCardHandler = (tag: Tag) => {
-        const handleOpenDeleteTagCard = () => {
-            setDeleteTagCardData(tag);
-        }
+  useEffect(
+    () => {
+      setIsRequestLoading(true)
 
-        return handleOpenDeleteTagCard;
-    };
+      fetchAllTags()
+    },
 
-    const getDeleteTagHandler = (tag: Tag | null) => {
-        const finishDeleteProcess = () => {
-            setDeleteTagCardData(null);
+    [
+      page,
+      quantity,
+      orderColumn,
+      orderBy,
+      search
+    ]
+  )
 
-            fetchAllTags();
-        };
+  useEffect(
+    () => {
+      paginatedTags
+        ? setTags(paginatedTags.data)
+        : setTags([])
 
-        return () => {
-            if (!tag)
-                return;
+      const isCurrentPageEmpty = paginatedTags &&
+        paginatedTags.data.length <= 0 &&
+        paginatedTags.currentPage > 1
 
-            fetch(
-                `${API_URL}/tag/inactivate/${tag.id}`,
-                {
-                    method: "DELETE",
-                    headers: {
-                        "Authorization": `Bearer ${localStorage.getItem("token")}`
-                    }
-                }
-            )
-                .then(handleDeleteTagResponse)
-                .catch(console.error)
-                .finally(finishDeleteProcess);
-        }
-    };
+      if (isCurrentPageEmpty)
+        setPage(1)
+    },
 
-    const handleDeleteTagCardClose = () => {
-        setDeleteTagCardData(null);
-    };
+    [
+      paginatedTags
+    ]
+  )
 
-    const handleSearchChange = (newSearch: string) => {
-        setSearch(newSearch);
-    };
+  const noContentWarning = <Typography variant="body1">Não há tags cadastradas.</Typography>
 
-    useEffect(
-        () => {
-            setIsRequestLoading(true);
+  return (
+    <Box sx={{ position: "relative" }}>
+      <PaginatedContent<TagOrderColumn>
+        contentTitle="Gerenciar tags"
+        hidePaginationContent={tags.length <= 0}
+        isRequestLoading={isRequestLoading}
+        currentQuantity={tags.length}
+        totalQuantity={paginatedTags?.totalQuantity || 0}
+        noContent={noContentWarning}
 
-            fetchAllTags();
-        },
+        newSearch={search}
+        handleSearchChange={handleSearchChange}
 
-        [
-            page,
-            quantity,
-            orderColumn,
-            orderBy,
-            search
-        ]
-    );
+        quantityPerPage={quantity}
+        handleQuantityPerPageChange={handleQuantityChange}
 
-    useEffect(
-        () => {
-            paginatedTags
-                ? setTags(paginatedTags.data)
-                : setTags([]);
+        page={page}
+        handlePageChange={handlePageChange}
+        lastPage={paginatedTags?.lastPage || 1}
 
-            const isCurrentPageEmpty = paginatedTags &&
-                paginatedTags.data.length <= 0 &&
-                paginatedTags.currentPage > 1;
+        orderBy={orderBy}
+        handleOrderByChange={handleOrderByChange}
 
-            if (isCurrentPageEmpty)
-                setPage(1);
-        },
+        orderColumns={[
+          ["id", "ID"],
+          ["name", "Nome"],
+          ["updatedAt", "Recentemente atualizado"]
+        ]}
+        handleOrderColumnChange={handleOrderColumnChange}
+        currentOrderColumn={orderColumn}
+      >
+        <AdminPanelTagTable
+          data={tags}
+          getDeleteHandler={getOpenDeleteTagCardHandler}
+        />
+      </PaginatedContent>
 
-        [
-            paginatedTags
-        ]
-    );
+      <AdminPanelAddContentFAB href="/admin/tags/0" />
 
-    const noContentWarning = <Typography variant="body1">Não há tags cadastradas.</Typography>;
+      <AdminPanelDeleteTagCard
+        tag={deleteTagCardData}
+        isOpen={!!deleteTagCardData}
+        handleClose={handleDeleteTagCardClose}
+        handleDelete={getDeleteTagHandler(deleteTagCardData)}
+      />
 
-    return (
-        <Box sx={{ position: "relative" }}>
-            <PaginatedContent<TagOrderColumn>
-                contentTitle="Gerenciar tags"
-                hidePaginationContent={tags.length <= 0}
-                isRequestLoading={isRequestLoading}
-                currentQuantity={tags.length}
-                totalQuantity={paginatedTags?.totalQuantity || 0}
-                noContent={noContentWarning}
+      <ErrorCard
+        isOpen={!!statusCode}
+        statusCode={statusCode}
+        handleClose={handleErrorClose}
+        reasons="Um erro inesperado ocorreu. Por favor, tente novamente mais tarde."
+      />
 
-                newSearch={search}
-                handleSearchChange={handleSearchChange}
-
-                quantityPerPage={quantity}
-                handleQuantityPerPageChange={handleQuantityChange}
-
-                page={page}
-                handlePageChange={handlePageChange}
-                lastPage={paginatedTags?.lastPage || 1}
-
-                orderBy={orderBy}
-                handleOrderByChange={handleOrderByChange}
-
-                orderColumns={[
-                    ["id", "ID"],
-                    ["name", "Nome"],
-                    ["updatedAt", "Recentemente atualizado"]
-                ]}
-                handleOrderColumnChange={handleOrderColumnChange}
-                currentOrderColumn={orderColumn}
-            >
-                <AdminPanelTagTable
-                    data={tags}
-                    getDeleteHandler={getOpenDeleteTagCardHandler}
-                />
-            </PaginatedContent>
-
-            <AdminPanelAddContentFAB href="/admin/tags/0" />
-
-            <AdminPanelDeleteTagCard
-                tag={deleteTagCardData}
-                isOpen={!!deleteTagCardData}
-                handleClose={handleDeleteTagCardClose}
-                handleDelete={getDeleteTagHandler(deleteTagCardData)}
-            />
-
-            <ErrorCard
-                isOpen={!!statusCode}
-                statusCode={statusCode}
-                handleClose={handleErrorClose}
-                reasons="Um erro inesperado ocorreu. Por favor, tente novamente mais tarde."
-            />
-
-            <SuccessCard
-                message="Tag deletada com sucesso!"
-                isOpen={showSuccessCard}
-                handleClose={() => setShowSuccessCard(false)}
-            />
-        </Box>
-    );
+      <SuccessCard
+        message="Tag deletada com sucesso!"
+        isOpen={showSuccessCard}
+        handleClose={() => setShowSuccessCard(false)}
+      />
+    </Box>
+  )
 }
